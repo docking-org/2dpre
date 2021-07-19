861d99627ade0bc81e45558a6dfb83be20496480
Ben <ben@tingle.org>
2021-07-19 11:47:37 -0700

    new scripts for depleting supplier codes

--- 2dload_new.py
+++ 2dload_new.py
@@ -222,6 +222,55 @@ if chosen_mode == "upload":
         print("operation completed successfully!")
     os.remove(psqlvars["source_f"])
 
+if chosen_mode == "deplete":
+    boolval = True if sys.argv[3].lower() == "true" else False
+    src = sys.argv[4]
+
+    dtype, dval = src.split("=")
+
+    if dtype == "catalog":
+        data_catid = call_psql(database_port, cmd="select cat_id from catalog where shortname = '{}'".format(dval), getdata=True)
+        cat_id = int(data_catid[1][0])
+
+        psqlvars= {
+            "cat_id" : cat_id,
+            "depleted_bool" : boolval
+        }
+
+        code = call_psql(database_port, psqlfile=BINDIR + "/psql/tin_deplete_catalog.pgsql", vars=psqlvars)
+        if code == 0:
+            print("operation completed successfully!")
+        pass
+
+    elif dtype == "file":
+        if not dval.endswith(".pre"):
+            print("expects a .pre file! try again")
+            sys.exit(1)
+
+        psqlvars = {
+            "source_f" : None,
+            "depleted_bool" : boolval
+        }
+
+        psqlvars["source_f"] = (os.environ.get("TEMPDIR") or "/local2/load") + "/" + str(database_port) + "_" + cat_shortname + "_deplete.txt"
+
+        deplete_src = open(psqlvars["source_f"], 'w')
+
+        with tarfile.open(source_f, mode='r:*') as pre_source:
+            for member in pre_source:
+                with pre_source.extractfile(member) as f:
+                    for line in f:
+                        tokens = line.strip().split()
+                        deplete_src.write(tokens[1] + "\n")
+        
+        deplete_src.close()
+
+        code = call_psql(database_port, psqlfile=BINDIR + "/psql/tin_deplete_sample.pgsql", vars=psqlvars)
+        if code == 0:
+            print("operation completed successfully!")
+        os.remove(psqlvars["source_f"])
+        pass
+
 
     
 
